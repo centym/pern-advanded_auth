2e9526cc0b11ea87f6660c30ea6bd4de7ab27c87
Yves Dumoulin <yves.centym@gmail.com>
2025-07-14 21:58:44 -0400

    centrer les pages login

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -29,7 +29,7 @@ const SignUpPage = () => {
 			initial={{ opacity: 0, y: 20 }}
 			animate={{ opacity: 1, y: 0 }}
 			transition={{ duration: 0.5 }}
-			className='max-w-md w-full bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl 
+			className='max-w-md w-full mx-auto bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl 
 			overflow-hidden'
 		>
 			<div className='p-8'>
